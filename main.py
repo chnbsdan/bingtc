e0d55a2f344707d672f1b62e89e578ccbb6cc25a
chnbsdan <chnbsdan@gmail.com>
2026-07-25 14:45:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from dimages import downloads
 
 if __name__ == "__main__":
     create()
-    downloads('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160&mkt=zh-CN')
+    downloads('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160&mkt=zh-CN')
